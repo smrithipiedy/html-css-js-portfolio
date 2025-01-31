8dd0c823b3be6b6b2fcd3df1a973478c1df9b12d
Smrithi P <smrithipiedy21@outlook.com>
2025-01-31 11:01:11 +0530

    updates for portfolio

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function smoothScroll(target) {
+  document.querySelector(target).scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+document.querySelectorAll('nav a, .menu-links a, footer a, .logo').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    smoothScroll(this.getAttribute('href'));
+  });
+});
